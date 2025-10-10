fb88f7d27baeb2c56499dbe9762d12e82f801bf9
mdenishchuk <denishchukmv@gmail.com>
2025-10-10 11:45:51 +0300

    DataGrid v1.0.8

--- DataGrid/Widget.js
+++ DataGrid/Widget.js
@@ -1,4 +1,9 @@
+// TODO
+
+
 // Преобразование данных из w
+
+
 const data = w.data.primaryData.items.map(item => {
     const obj = {};
     item.cols.forEach((col, i) => {
@@ -39,4 +44,66 @@ $(`#table-${w.general.renderTo}`).dxDataGrid({
     }
 
 });
+
+
+
+function pullPopup(e){
+    const rect = e.getBoundingClientRect();
+    document.documentElement.style.setProperty('--hover-transform', `translate(0px,0px)`)
+    document.documentElement.style.setProperty('--hover-top', `${rect.top + rect.height}px`)
+    document.documentElement.style.setProperty('--hover-left', `${rect.left}px`)
+}
+
+$("#"+ w.general.renderTo).on("mouseover", ".dx-datagrid-filter-row > td", function() {
+    pullPopup(this);
+});
+
+$("#"+ w.general.renderTo).on("mouseover", ".dx-header-filter", function() {
+    pullPopup(this);
+});
+
+
+
+let styleGlogal = document.getElementById('DevExpres_popup');
+if (!styleGlogal) {
+    styleGlogal = document.createElement('style');
+    styleGlogal.id = 'DevExpres_popup';
+    styleGlogal.innerHTML = `
+    :root {
+      --hover-transform: translate(0px,0px);
+      --hover-top: 0px;
+      --hover-left: 0px;
+
+      --popup-transform: translate(0px,0px) scale(1);
+      --popup-top: 0px;
+      --popup-left: 0px;
+    }
+    .dx-overlay-content.dx-popup-normal.dx-resizable.dx-dropdowneditor-overlay {
+        height: 300px !important;
+    }
+
+    .dx-overlay-wrapper {
+
+        position: absolute  !important;
+
+        top: var(--hover-top) !important;
+        left: var(--hover-left) !important;
+        transform: var(--hover-transform) !important;
+
+
+    }
+
+    .dx-overlay-content.dx-popup-normal.dx-resizable.dx-dropdowneditor-overlay,
+    .dx-overlay-content.dx-inner-overlay.dx-resizable.dx-context-menu.dx-datagrid.dx-cell-focus-disabled.dx-filter-menu.dx-menu-base {
+        display: block !important;
+        transform: var(--popup-transform) !important;
+        top: var(--popup-top) !important;
+        left: var(--popup-left) !important;
+        transform-origin: left top !important;
+
+    }
+
+    `;
+    document.head.append(styleGlogal);
+}
 
